d0c8d6a2b13e53191701e5f3af9d0a78198fa18e
Ömer Oğuz Çelikel <omeroguz.celikel@tebarf.com.tr>
2023-06-01 15:57:33 +0300

    solved

--- 9. Palindrome Number.playground/Contents.swift
+++ 9. Palindrome Number.playground/Contents.swift
@@ -4,9 +4,40 @@ import UIKit
 //Given an integer x, return true if x is a palindrome, and false otherwise.
 
 func isPalindrome(_ x: Int) -> Bool {
+    
+    // minus ones will not be palindrome -121 != 121-
+    if x < 0 {
+        return false
+    }
+    
     let intArray = intToArr(x)
-    print("intArray: \(intArray)")
-    return false
+    let count = intArray.count
+    
+    print("3/2 \(3/2)")
+    
+    // checks until middle of count
+    // look first and last
+    for i in 0..<count/2 {
+        /*
+         FOR 0
+         
+         checks firsts and lasts
+         check: 987
+         count 3
+         index 0 = 9
+         index 3-0-1 -> 2: Gives last =  7
+        
+         FOR 1
+         index 1: 8
+         index 3-1-1 ->: 1 = 8
+         
+         
+         */
+        if intArray[i] != intArray[count - i - 1] {
+            return false
+        }
+    }
+    return true
 }
 
 func intToArr(_ x: Int) -> Array<Int> {
@@ -15,7 +46,7 @@ func intToArr(_ x: Int) -> Array<Int> {
 }
 
 
-isPalindrome(123)
+isPalindrome(121)
 
 //let num = 12345
 //let array = String(num).compactMap({$0.wholeNumberValue})
